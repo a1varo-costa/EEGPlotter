2460ef575825d52bff85b0d7f7f673648c1137c1
a1varo-costa <63411551+a1varo-costa@users.noreply.github.com>
2020-08-21 11:41:56 -0300

    add BlinkRect to keyboard tab

--- app/gui.py
+++ app/gui.py
@@ -1,14 +1,17 @@
 from .ui import mainWindowUI as mainWinUI
-from .   import plotter
+from .   import plotter, rect
 
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QGridLayout, QLabel, QSizePolicy
 
 
 class MainUI(QMainWindow):
-    def __init__(self, stream, sfreq):
+    def __init__(self, stream, sfreq, settings):
         super().__init__()
         self.stream = stream
         self.samplingFreq = sfreq
+
+        self.settings = settings
+        
         self._loadUI()
     
     def _loadUI(self):
@@ -16,3 +19,11 @@ class MainUI(QMainWindow):
         self.ui.setupUi(self)
 
         self.plt = plotter.Plotter(self.ui, self.stream, self.samplingFreq)
+        
+        self.rectGridLayout = QGridLayout(self.ui.keyboardWidget)
+
+        for setting in self.settings:
+            self.rectGridLayout.addWidget(rect.BlinkRect(label=setting[0],  
+                                                         freq =setting[1]), 
+                                          setting[2], setting[3])
+
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     app = QApplication([])
 
     stream = ser.SerialReader('COM4', 9600, 0.1, 1024)
-    plot = gui.MainUI(stream, 1/(30*10e-3))
+    settings = [('A', 3, 0, 0), ('B', 6, 0, 1)]
+    plot = gui.MainUI(stream, 1/(30*10e-3), settings)
     plot.show()
 
     sys.exit(app.exec_())
